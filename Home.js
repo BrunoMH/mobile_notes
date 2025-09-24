15d1b03f6991636ede9267103ee61bd40e36bcb9
Bruno_MH <brunomh141a@gmail.com>
2025-09-24 00:20:23 -0600

    basic navigation added with an add button for new note

---
+++ Home.js
@@ -0,0 +1,92 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function Home({ navigation }) {
+  const createNew = () => {
+    navigation.navigate('Note');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>notes</Text>
+      </View>
+
+      {/* searcher or notes */}
+      <View style={styles.placeholder}>
+        <Ionicons name="search" size={20} color="#000" style={styles.icon} />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Placeholder..."
+          onChangeText={(text) => console.log(text)}
+        />
+      </View>
+
+      {/* Floating button */}
+      <View style={styles.addButton}>
+        <Pressable style={styles.createNote} onPress={createNew}>
+          <Ionicons name="add" size={50} color="#000" />
+        </Pressable>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 50,
+  },
+  header: {
+    marginTop: 50,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  placeholder: {
+    width: 320,
+    height: 50,
+    backgroundColor: '#E0BB00',
+    borderColor: '#69665b',
+    borderWidth: 2,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  inputText: {
+    flex: 1,
+    width: '100%',
+  },
+  icon: {
+    marginRight: 8,
+  },
+  createNote: {
+    width: 70,
+    height: 70,
+    backgroundColor: '#E0BB00',
+    borderRadius: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 130,
+    right: 30,
+  },
+});
